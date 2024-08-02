00007a4833238d3a2ddb1d8071cd30d2239562e5
Aashish Panthee <aashishpanthee060@gmail.com>
2024-08-03 00:35:20 +0545

    chore:init

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/react-in-jsx-scope': 0,
+    'import/no-cycle': 0,
+    'react/no-unescaped-entities': 0,
+    'import/prefer-default-export': 0,
+    'react/require-default-props': 0,
+    'react/no-array-index-key': 0,
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+  },
+};
